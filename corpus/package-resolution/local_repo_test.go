246ea44b9ba706c184f3ee9b4a086dac39edaf92
azinneera <azinneera@gmail.com>
2026-07-03 13:31:55 +0530

    Update tests and address review suggestions

--- corpus/package-resolution/local_repo_test.go
+++ corpus/package-resolution/local_repo_test.go
@@ -25,6 +25,7 @@ package packageresolution
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"fmt"
 	"os"
@@ -34,6 +35,7 @@ import (
 	"strings"
 	"sync"
 	"testing"
+	"time"
 
 	"golang.org/x/tools/txtar"
 )
@@ -129,7 +131,17 @@ func runScenario(t *testing.T, scenarioDir, projectDir string) {
 func runBalRun(t *testing.T, projectDir, balEnvDir string) (stdout, stderr string) {
 	t.Helper()
 
-	cmd := exec.Command(pkgResBalBin, "run", projectDir)
+	deadline, ok := t.Deadline()
+	var ctx context.Context
+	var cancel context.CancelFunc
+	if ok {
+		ctx, cancel = context.WithDeadline(context.Background(), deadline)
+	} else {
+		ctx, cancel = context.WithTimeout(context.Background(), 60*time.Second)
+	}
+	defer cancel()
+
+	cmd := exec.CommandContext(ctx, pkgResBalBin, "run", projectDir)
 	cmd.Dir = pkgResRepoRoot
 
 	// Build subprocess environment: inherit current env, then override BAL_ENV.
